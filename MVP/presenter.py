9893668947b72607ea18af68a16fcf15966953d7
Joseph Park <emjosephpark.gmail.com>
2025-02-13 10:38:30 -0500

    Added skeleton functions

--- MVP/presenter.py
+++ MVP/presenter.py
@@ -9,8 +9,21 @@ class Presenter:
         self.model = model
         self.view = view
         self.view.set_exit_callback(self.on_exit_requested)
+    
+    def new_project(self):
+        '''
+        Opens New Project UI when User Clicks on "New Project"
+        '''
+    
+    def saved_project(self):
+        '''
+        Opens View Saved Projects UI when User clicks on "View Saved"
+        '''
 
     def on_exit_requested(self):
+        '''
+        Exits Application via Message Box
+        '''
         do_exit = QMessageBox.question(self.view, "Exit", "Are you sure you want to exit?",
                                        QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
 
