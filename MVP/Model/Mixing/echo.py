68d13f05489e3c0f2ceccd6c7b0aeb26f5e5be74
lin1397 <lin1397@purdue.edu>
2025-04-29 11:19:14 -0400

    echo

--- MVP/Model/Mixing/echo.py
+++ MVP/Model/Mixing/echo.py
@@ -1,12 +1,13 @@
 import numpy as np
+import copy
 
 def apply_echo(chunks, decay, sample_rate, frame):
-    last_echo_chunk = chunks[:frame][:]
+    last_echo_chunk = chunks[:frame][:].copy()
     # print(last_echo_chunk)
     last_echo_chunk *= (decay * decay)
     # print(last_echo_chunk)
     
-    new_echo_chunk = chunks[frame:frame*2][:]
+    new_echo_chunk = chunks[frame:frame*2][:].copy()
     new_echo_chunk *= decay
     
     new_echo_chunk = np.vstack((new_echo_chunk, last_echo_chunk))
@@ -16,7 +17,7 @@ def apply_echo(chunks, decay, sample_rate, frame):
         last_echo_chunk *= decay
         #print(last_echo_chunk)
         
-        new_echo_chunk = chunks[chunk*frame:(chunk+1)*frame][:]
+        new_echo_chunk = chunks[chunk*frame:(chunk+1)*frame][:].copy()
         new_echo_chunk *= decay
         #print(new_echo_chunk)
         
@@ -24,15 +25,15 @@ def apply_echo(chunks, decay, sample_rate, frame):
         
     return new_echo_chunk
 
-# if __name__ == "__main__":
-#     frame = 25
-#     total_chunks = 5
-#     decay = 0.5
-#     sample_rate = 44100
-#     chunks = np.ones((4, 25)).astype(float)
+if __name__ == "__main__":
+    frame = 25
+    total_chunks = 5
+    decay = 0.5
+    sample_rate = 44100
+    chunks = np.ones((4, 25)).astype(float)
     
-#     print("Original Chunks:\n", chunks)
+    print("Original Chunks:\n", chunks)
 
-#     echoed = apply_echo(chunks, decay, sample_rate, frame)
+    echoed = apply_echo(chunks, decay, sample_rate, frame)
 
-#     print("\nEchoed Output:\n", echoed)
+    print("\nEchoed Output:\n", echoed)
